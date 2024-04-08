5baacfb278d69f6d8dde5c9d76fa4bce64353612
Cesar <cesar@cesar.wtf>
2024-04-08 17:17:44 +0200

    First working version

--- queries/products.sql
+++ queries/products.sql
@@ -1,10 +1,26 @@
+{@const search = param("search", "")}
+{@const open_source = param("open_source", "all")}
+
 select *,
        string_to_array(extensions, ',') as extensions_array
 from read_csv_auto('queries/products.csv', header=true)
 
-{@const search = param("search", "")}
+{#if search || open_source == true}
+    where
+{/if}
 
 {#if search}
-    where lower(name) like '%' || lower({search}) || '%'
-       or lower(description) like '%' || lower({search}) || '%'
+    lower(name) like '%' || lower({search}) || '%'
+    or lower(description) like '%' || lower({search}) || '%'
+    or lower(extensions) like '%' || lower({search}) || '%'
 {/if}
+
+{#if search && open_source == true}
+    and
+{/if}
+
+{#if open_source == true}
+    open_source = true
+{/if}
+
+order by category asc
