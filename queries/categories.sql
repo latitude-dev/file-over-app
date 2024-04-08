4f58dd5d69834e96c54ddd9ad5f9c100b5756460
Cesar <cesar@cesar.wtf>
2024-04-08 12:03:26 +0200

    wip

--- queries/categories.sql
+++ queries/categories.sql
@@ -1,3 +1,3 @@
 select distinct category as category
-from { ref('products') }
+from read_csv_auto('queries/products.csv', header=true)
 order by 1 asc
--- queries/products.sql
+++ queries/products.sql
@@ -1,3 +1,10 @@
 select *,
        string_to_array(extensions, ',') as extensions_array
 from read_csv_auto('queries/products.csv', header=true)
+
+{@const search = param("search", "")}
+
+{#if search}
+    where lower(name) like '%' || lower({search}) || '%'
+       or lower(description) like '%' || lower({search}) || '%'
+{/if}
